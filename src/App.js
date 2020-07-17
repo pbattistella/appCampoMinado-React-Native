6b34f8e5ef53c6269733a0f2e0f213c5476a523e
pbattistella <battistellaa@gmail.com>
2020-07-17 18:31:44 -0300

    Com os campos criados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {Platform, StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 import params from './params'
+import Field from './components/Field'
 
 
 export default class App extends Component{
@@ -11,6 +12,15 @@ export default class App extends Component{
         <Text style = {styles.welcome} >Tamanho:
           {params.getRowsAmount()} X {params.getColumnsAmount()} 
         </Text>
+        <Field/>
+        <Field opened />
+        <Field opened nearMines = {1} />
+        <Field opened nearMines = {2} />
+        <Field opened nearMines = {3} />
+        <Field opened nearMines = {4} />
+        <Field opened nearMines = {5} />
+        <Field opened nearMines = {6} />
+        <Field mined opened/>
       </View>
     )
   }
